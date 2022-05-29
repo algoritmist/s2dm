8908d02a09a63ce0e18b594e5ceab19a24c75c3d
Viacheslav Baranov <barash.2018@yandex.ru>
2022-05-30 00:41:23 +0300

    Changed build

--- texbuild.sh
+++ texbuild.sh
@@ -1,1 +1,15 @@
-jupyter nbconvert --to latex --no-input HamiltonianCycle.ipynb
+#!/bin/bash
+
+echo "Enter path to file without extension: "
+
+read path
+
+mkdir solution
+
+
+cp $path solution
+
+cp solution
+jupyter nbconvert --to latex --no-input solution/*.ipynb
+
+find . -name "*.tex" -exec sed -i '1 a \\ \\usepackage[T2A]{fontenc}\n\\usepackage[utf8]{inputenc}\n\\usepackage[russian]{babel}' {} +
